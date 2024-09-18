d27d8d49c684b33523ffda2ad9616a334d5ca846
hqr <xxx>
2024-09-18 13:37:49 +0800

    add work  ret

--- include/Tdesign.h
+++ include/Tdesign.h
@@ -8,17 +8,40 @@
 namespace bhtools {
 
 
-// 执行模板职责链判断函数-如果选择继续执行则职责链不会停止
+// 执行模板职责链判断函数-可返回处理函数类型-带返回值
+template<typename Tclass,typename Treturn,size_t now>
+struct Twork_action_ret
+{
+    // 执行类对象的 accept 判断函数-如果为真则代表接收处理并结束
+    // 返回值为 std::pair 类型-第一个类型是 bool 决定是否继续处理-第二个类型为外部处理函数自定义类型
+    template<typename ...Targ>
+    static std::pair<bool,Treturn> action(Tclass obj,Targ ...arg) 
+    {
+        auto it = std::get<now -1>(obj);
+        auto pair = it.accept(arg...);
+        if(pair.first) { return pair; }
+        return Twork_action_ret<Tclass,Treturn,now -1>::action(obj,arg...);
+    }
+};
+
+// 退出模板职责链-带返回值
+template<typename Tclass,typename Treturn>
+struct Twork_action_ret<Tclass,Treturn,0>
+{
+    template<typename ...Targ>
+    static std::pair<bool,Treturn> action(Tclass obj,Targ ...arg) { return {}; }
+};
+
+// 处理函数无返回值的特殊处理-返回布尔值表示是否接收处理
 template<typename Tclass,size_t now>
 struct Twork_action
 {
-    // 执行类对象的next判断函数-如果为真则代表继续处理
     template<typename ...Targ>
     static bool action(Tclass obj,Targ ...arg) 
     {
         auto it = std::get<now -1>(obj);
-        if(it.next(arg...)) { return Twork_action<Tclass,now -1>::action(obj,arg...); }
-        return true;
+        if(it.accept(arg...)) { return true; }
+        return Twork_action<Tclass,now -1>::action(obj,arg...);
     }
 };
  
@@ -34,8 +57,18 @@ struct Twork_action<Tclass,0>
 template<typename ...Tclass>
 struct Twork_chain
 {
-    // 开始执行职责链-传入具体处理函数的参数-处理顺序为函数对象的倒序执行-确保第一个执行的函数放在最后
-    // 返回值 [true: 职责被接收]-[false: 未找到处理函数]
+    // 开始执行职责链-传入具体处理函数的参数-处理顺序为函数对象的倒序执行-确保第一个执行的函数放在最后-带返回值
+    template<typename Treturn,typename ...Targ>
+    std::pair<bool,Treturn> start(Targ ...arg)
+    { 
+        return Twork_action_ret<
+                std::tuple<Tclass...>,
+                Treturn,
+                std::tuple_size<std::tuple<Tclass...>>::value
+            >::action(_objs,arg...); 
+    }
+
+    // 开始执行职责链-无处理函数返回值
     template<typename ...Targ>
     bool start(Targ ...arg)
     { 
--- test/main_design.cpp
+++ test/main_design.cpp
@@ -13,18 +13,121 @@
 
 #include "Tdesign.h"
 #include "Tlog.h"
+#include "Ffm.h"
 // #include "Ftime.h"
 
 using namespace bhtools;
 
+
+namespace Test1 {
 
 // 模式1-使用父类继承-可减少每个处理类需要编写的代码量
 class TestBase
 {
 public:
     template<typename ...Targ>
-    bool next(Targ ...arg) { return process(arg...); }
-    bool process() { return true; };
+    std::pair<bool,std::string> accept(Targ ...arg) { return process(arg...); }
+    std::pair<bool,std::string> process() { return {}; };
+    virtual std::pair<bool,std::string> process(std::string name,int score) = 0;
+};
+
+class TestA : public TestBase
+{
+public:
+    std::pair<bool,std::string> process(std::string name,int score) override
+    {
+        if(score >= 90)
+        {
+            std::string ret = Fffm("{}同学的得分是{}--属于A级")(name,score);
+            return std::make_pair(true,ret);
+        }
+        return std::make_pair(false,"");
+    }
+};
+
+class TestB : public TestBase
+{
+public:
+    std::pair<bool,std::string> process(std::string name,int score) override
+    {
+        if(score >= 75)
+        {
+            std::string ret = Fffm("{}同学的得分是{}--属于B级")(name,score);
+            return std::make_pair(true,ret);
+        }
+        return std::make_pair(false,"");
+    }
+};
+
+class TestC : public TestBase
+{
+public:
+    std::pair<bool,std::string> process(std::string name,int score)
+    {
+        if(score >= 60)
+        {
+            std::string ret = Fffm("{}同学的得分是{}--属于C级")(name,score);
+            return std::make_pair(true,ret);
+        }
+        return std::make_pair(false,"");
+    }
+};
+
+class TestD : public TestBase
+{
+public:
+    std::pair<bool,std::string> process(std::string name,int score)
+    {
+        if(score < 60)
+        {
+            std::string ret = Fffm("{}同学的得分是{}--属于D级")(name,score);
+            return std::make_pair(true,ret);
+        }
+        return std::make_pair(false,"");
+    }
+};
+
+
+} // Test1
+
+
+
+void test_1()
+{
+    using namespace Test1;
+    {
+        vlogi("== 1 ==");
+        Twork_chain<TestD,TestC,TestB,TestA> work;
+        {
+            auto ret = work.start<std::string>("黄一衣",98);
+            vlogd($(ret.first) $(ret.second));
+        }
+        {
+            auto ret = work.start<std::string>("周某星",76);
+            // vlogd($(ok));
+            vlogd($(ret.first) $(ret.second));
+        }
+        {
+            auto ret = work.start<std::string>("雷雷子",62);
+            vlogd($(ret.first) $(ret.second));
+        }
+        {
+            auto ret = work.start<std::string>("渣渣辉",53);
+            vlogd($(ret.first) $(ret.second));
+        }
+    }
+}
+
+
+namespace Test2 {
+
+
+class TestBase
+{
+public:
+    template<typename ...Targ>
+    bool accept(Targ ...arg) { return process(arg...); }
+    bool process() { return false; };
     virtual bool process(std::string name,int score) = 0;
 };
 
@@ -35,10 +138,11 @@ public:
     {
         if(score >= 90)
         {
-            std::cout<<name<<"同学的得分是"<<score<<"--属于A级"<<std::endl;
-            return false;
+            std::string ret = Fffm("{}同学的得分是{}--属于A级")(name,score);
+            vlogd($(ret));
+            return true;
         }
-        return true;
+        return false;
     }
 };
 
@@ -49,10 +153,11 @@ public:
     {
         if(score >= 75)
         {
-            std::cout<<name<<"同学的得分是"<<score<<"--属于B级"<<std::endl;
-            return false;
+            std::string ret = Fffm("{}同学的得分是{}--属于B级")(name,score);
+            vlogd($(ret));
+            return true;
         }
-        return true;
+        return false;
     }
 };
 
@@ -63,10 +168,11 @@ public:
     {
         if(score >= 60)
         {
-            std::cout<<name<<"同学的得分是"<<score<<"--属于C级"<<std::endl;
-            return false;
+            std::string ret = Fffm("{}同学的得分是{}--属于C级")(name,score);
+            vlogd($(ret));
+            return true;
         }
-        return true;
+        return false;
     }
 };
 
@@ -75,27 +181,32 @@ class TestD : public TestBase
 public:
     bool process(std::string name,int score)
     {
-        if(score < 60)
+        if(score > 0 && score < 60)
         {
-            std::cout<<name<<"同学的得分是"<<score<<"--属于D级"<<std::endl;
-            return false;
+            std::string ret = Fffm("{}同学的得分是{}--属于D级")(name,score);
+            vlogd($(ret));
+            return true;
         }
-        return true;
+        return false;
     }
 };
 
+} // Test2
 
-void test_1()
+
+
+void test_2()
 {
+    using namespace Test2;
     {
         vlogi("== 1 ==");
         Twork_chain<TestD,TestC,TestB,TestA> work;
         {
-            bool ok = work.start("黄衣",98);
+            bool ok = work.start("黄一衣",98);
             vlogd($(ok));
         }
         {
-            bool ok = work.start("周某",76);
+            bool ok = work.start("周某星",76);
             vlogd($(ok));
         }
         {
@@ -104,18 +215,21 @@ void test_1()
         }
         {
             bool ok = work.start("渣渣辉",53);
+            vlogd($(ok));
+        }        
+        {
+            bool ok = work.start("猪猪侠",-13);
             vlogd($(ok));
         }
     }
     {
-        vlogi("== 2 ==");
         Twork_chain<TestA,TestB,TestC,TestD> work;
         {
-            bool ok = work.start("黄衣",98);
+            bool ok = work.start("黄一衣",98);
             vlogd($(ok));
         }
         {
-            bool ok = work.start("周某",76);
+            bool ok = work.start("周某星",76);
             vlogd($(ok));
         }
         {
@@ -125,11 +239,15 @@ void test_1()
         {
             bool ok = work.start("渣渣辉",53);
             vlogd($(ok));
+        }        
+        {
+            bool ok = work.start("猪猪侠",-13);
+            vlogd($(ok));
         }
     }
 }
+namespace Test3 {
 
-// 模式2-处理类单独处理模板职责链需要的鸭子函数
 struct TestStringData
 {
     int height;    
@@ -137,73 +255,77 @@ struct TestStringData
     std::string info;     
 };
 
+// 模式2-处理类单独处理模板职责链需要的鸭子函数
 class TestStringA
 {
 public:
     template<typename ...Targ>
-    bool next(Targ ...arg) { return process(arg...); }
+    std::pair<bool,int> accept(Targ ...arg) { return process(arg...); }
 
     template<typename ...Targ>
-    bool process(TestStringData data)
+    std::pair<bool,int> process(TestStringData data)
     {
         if(data.info == "长方形")
         {
             int ret = data.height * data.width;
-            std::cout<<"图形是"<<data.info<<"图形面积是: "<<ret<<std::endl;
-            return false;
+            return std::make_pair(true,ret);
         }
-        return true;
+        return std::make_pair(false,0);
     }
-    bool process() { return true; }
+    std::pair<bool,int> process() { return {}; }
 };
 
 class TestStringB
 {
 public:
     template<typename ...Targ>
-    bool next(Targ ...arg) { return process(arg...); }
+    std::pair<bool,int> accept(Targ ...arg) { return process(arg...); }
 
     template<typename ...Targ>
-    bool process(TestStringData data)
+    std::pair<bool,int> process(TestStringData data)
     {
         if(data.info == "三角形")
         {
             int ret = data.height * data.width / 2;
-            std::cout<<"图形是"<<data.info<<"图形面积是: "<<ret<<std::endl;
-            return false;
+            return std::make_pair(true,ret);
         }
-        return true;
+        return std::make_pair(false,0);
     }
-    bool process() { return true; }
+    std::pair<bool,int> process() { return {}; }
 };
 
-void test_2()
+
+} // Test3
+
+
+void test_3()
 {
-    vlogi("== test_2 ==");
+    vlogi("== test_3 ==");
+    using namespace Test3;
     Twork_chain<TestStringB,TestStringA> work;
     {
         TestStringData a;
         a.info = "长方形";
         a.height = 10;
         a.width = 5;
-        bool ok = work.start(a);
-        vlogd($(ok));
+        auto ret = work.start<int>(a);
+        vlogd($(ret.first) $(ret.second));
     }
     {
         TestStringData a;
         a.info = "三角形";
         a.height = 10;
         a.width = 5;
-        bool ok = work.start(a);
-        vlogd($(ok));
+        auto ret = work.start<int>(a);
+        vlogd($(ret.first) $(ret.second));
     }
     {
         TestStringData a;
         a.info = "圆形";
         a.height = 10;
         a.width = 5;
-        bool ok = work.start(a);
-        vlogd($(ok));
+        auto ret = work.start<int>(a);
+        vlogd($(ret.first) $(ret.second));
     }
 }
 
@@ -211,7 +333,7 @@ int main(int argc, char *argv[])
 {
     test_1();   
     test_2();   
-    // test_3();   
+    test_3();   
     // test_4();   
     // test_5();
     // test_6();
