d6d45179a99e88baca6459b45d81af046c37dfa0
BH <2655279792@qq.com>
2025-08-08 16:05:13 +0800

    change any

--- include/Tany.h
+++ include/Tany.h
@@ -16,49 +16,49 @@ struct Tany
     struct type_base
     {
         virtual ~type_base() {}
-        virtual type_base* make() = 0;
+        virtual type_base* get() = 0;
+        virtual type_base* copy() = 0;
     };
 
     // 类型保存-存储具体值得模板类-用于生成指针
     template<typename T>
     struct type_pack : public type_base
     {
-        // 传入需要保存的值
-        type_pack(const T &val) : _val(val) {}
+        type_pack(const T &val) : _val(val) { }
+
+        // 返回自身
+        inline type_base* get() override { return this; }
 
         // 将值内容分配到指针
-        type_base* make() override 
-        { return new type_pack<T>(_val); }
+        inline type_base* copy() override { return new type_pack<T>(_val); }
 
         T _val;     // 动态保存的值
     };
 
+
     // 默认无数据
-    Tany() : _type(typeid(void)) {}
+    Tany() : _type(&typeid(void)) {}
 
     // 有数据
     template<typename T>
-    Tany(const T &val) : _type(typeid(T))
+    Tany(const T &val) 
+    { set(val); }
+
+    Tany(const char *val) 
     { set(val); }
 
     // 拷贝构造只复制保存值
-    Tany(const Tany &any) : _type(typeid(any._type))
+    Tany(const Tany &any)
     { set(any); }
 
-    // 字符串统一转成std::string类型
-    Tany(const char *val) : _type(typeid(std::string))
-    { set(std::string(val)); }
-
-    // 作用同构造
-    Tany& operator=(const char *val)
-    { set(std::string(val)); return *this; }
-
-    // 作用同构造
+    // 重载赋值
     template<typename T>
     Tany& operator=(const T &val)
     { set(val); return *this; }
 
-    // 作用同构造
+    Tany& operator=(const char *val)
+    { set(val); return *this; }
+
     Tany& operator=(const Tany &any)
     { set(any); return *this; }
 
@@ -67,11 +67,11 @@ struct Tany
 
     // 查询使用的类型
     template<typename T>
-    bool use() { return _type == std::type_index(typeid(T)) && _val != nullptr; }
+    bool use() { return (*_type == typeid(T)); }
 
     // 直接返回保存值-需要先查询
     template<typename T>
-    T& value() { return (static_cast<type_pack<T>*>(_val))->_val; }
+    const T& value() { return (static_cast<type_pack<T>*>(_val))->_val; }
 
 
     // internal
@@ -79,8 +79,8 @@ struct Tany
     template<typename T>
     void set(const T &val)
     {
-        _type = std::type_index(typeid(T));
-        _val = type_pack<T>(val).make();
+        _type = &typeid(T);
+        _val = new type_pack<T>(val);
     }
 
     // 释放当前保存指针并拷贝传入保存值
@@ -94,19 +94,19 @@ struct Tany
     // 重置状态和释放保存值
     void reset()
     {
-        _type = std::type_index(typeid(void));
+        _type = &typeid(void);
         if(_val) { delete _val; _val = nullptr; }
     }
  
     // 用于拷贝构造复制对象保存值
     type_base* copy() const
     { 
-        if(_val) { return _val->make(); }
+        if(_val) { return _val->copy(); }
         return nullptr;
     }
 
-    std::type_index _type;      // 保存值得类型
-    type_base *_val = nullptr;  // 保存值的无类型指针
+    const std::type_info *_type = nullptr;  // 保存值得类型
+    type_base *_val = nullptr;              // 保存值的无类型指针
 };
 
 
--- test/main_any.cpp
+++ test/main_any.cpp
@@ -2,133 +2,453 @@
 #include <iostream>
 #include <optional>
 
-#include "Tlog.h"
-#include "Tany.h"
-#include "Ftest.h"
-#include "Ftime.h"
-
-using namespace bhtools;
-using namespace std;
+#include "bhtools.h"
 
 
 struct T1
 {
     T1() {}
     T1(int i) :_i(i) {}
-    T1(const string &s) :_s(s) {}
-    T1(int i,const string &s) :_i(i),_s(s) {}
+    T1(const std::string &s) :_s(s) {}
+    T1(int i,const std::string &s) :_i(i),_s(s) {}
     ~T1()
     {
         vlogw($("T1") $(_i) $(_s));
     }
     int _i = -1;
-    string _s;
+    std::string _s;
 };
 
 
 void test_1()
 {
+    // 测试类型有效
+    int a1 = 123;
+    short a2 = 456;
+    size_t a3 = 789;
+    const char *a4 = "hellow";
+    std::string a5 = "world";
+    bool a6 = true;
+    char a7 = 'k';
+    float a8 = 123.456;
+    double a9 = 666.666;
+
     {
-        Tany any;
-        vlogd($(any._type.name()));
+        bhtools::Tany any;
+        vlogw($("null"));
         vlogd($(any.use<void>()));
-    }
-    {
-        Tany any(123);
         vlogd($(any.use<int>()));
-        vlogd($(any._type.name()));
-
-        if(any.use<int>())
-        {
-            auto s = any.value<int>();
-            vlogd($(s));
-        }
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any(123.456);
-        vlogd($(any._type.name()));
-        vlogd($(any.value<double>()));
+        bhtools::Tany any(a1);
+        vlogw($(a1));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any("hellow");
-        vlogd($(any._type.name()));
-        vlogd($(any.value<std::string>()));
+        bhtools::Tany any(a2);
+        vlogw($(a2));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any(T1(123));
-        vlogd($(any._type.name()));
-        vlogd($(any.value<T1>()._i) $(any.value<T1>()._s));
+        bhtools::Tany any(a3);
+        vlogw($(a3));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any(T1(100,"hhq"));
-        vlogd($(any._type.name()));
-        vlogd($(any.value<T1>()._i) $(any.value<T1>()._s));
+        bhtools::Tany any(a4);
+        vlogw($(a4));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any1(123);
-        Tany any2("swq");
-        vlogd($(any1._type.name()) $(any2._type.name()) );
-        vlogd( $(any1.value<int>()) $(any2.value<std::string>()) );
-
-        Tany any3(any1);
-        Tany any4(std::move(any2));
-
-        vlogd($(any1._type.name()) $(any2._type.name()) $(any3._type.name()) $(any4._type.name()));
-        vlogd( $(any1.value<int>()) $(any2.value<std::string>()) $(any3.value<int>()) $(any4.value<std::string>()));
+        bhtools::Tany any(a5);
+        vlogw($(a5));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Tany any1(123);
-        Tany any2("swq");
-        Tany any3 = any1;
-        Tany any4;
-
-        vlogd($(any1._type.name()) $(any2._type.name()) $(any3._type.name()) $(any4._type.name()));
-        
-        any4 = any2;
-
-        vlogd($(any1._type.name()) $(any2._type.name()) $(any3._type.name()) $(any4._type.name()));
-
-        vlogd( $(any1.value<int>()) $(any2.value<std::string>()) $(any3.value<int>()) $(any4.value<std::string>()));
+        bhtools::Tany any(a6);
+        vlogw($(a6));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
+    {
+        bhtools::Tany any(a7);
+        vlogw($(a7));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(a8);
+        vlogw($(a8));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(a9);
+        vlogw($(a9));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+
 }
+
 
 void test_2()
 {
-    int count = 1000*10000;
-
+    // 测试具体类型
     {
-        Ftimel t;
-        Tany any;
-        for(int i=0;i<count;i++)
-        {
-            any = i;
-        }
-        vlogd($(t.to_string()));
+        bhtools::Tany any;
+        vlogw($("void"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
     {
-        Ftimel t;
-        Tany any;
-        for(int i=0;i<count;i++)
-        {
-            Tany any1;
-            any1 = i;
-            any = any1;
-        }
-        vlogd($(t.to_string()));
+        bhtools::Tany any(1234);
+        vlogw($("int"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(1234L);
+        vlogw($("long"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<long>()));
+        vlogd($(any.use<long long>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(1234LL);
+        vlogw($("long long"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<long>()));
+        vlogd($(any.use<long long>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(1234UL);
+        vlogw($("size_t"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<long>()));
+        vlogd($(any.use<long long>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(123.456);
+        vlogw($("double"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(123.456f);
+        vlogw($("float"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any("hellow");
+        vlogw($("const char *"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(std::string("hellow"));
+        vlogw($("string"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any(true);
+        vlogw($("bool"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
+    }
+    {
+        bhtools::Tany any('k');
+        vlogw($("char"));
+        vlogd($(any.use<void>()));
+        vlogd($(any.use<int>()));
+        vlogd($(any.use<short>()));
+        vlogd($(any.use<size_t>()));
+        vlogd($(any.use<const char *>()));
+        vlogd($(any.use<std::string>()));
+        vlogd($(any.use<bool>()));
+        vlogd($(any.use<char>()));
+        vlogd($(any.use<float>()));
+        vlogd($(any.use<double>()));
     }
 
 }
 
 void test_3()
 {
+    // 获取值
+    int a1 = 123;
+    short a2 = 456;
+    size_t a3 = 789;
+    const char *a4 = "hellow";
+    std::string a5 = "world";
+    bool a6 = true;
+    char a7 = 'k';
+    float a8 = 123.456;
+    double a9 = 666.666;
 
+    {
+        bhtools::Tany any(a1);
+        vlogd($(any.use<int>()) $(any.value<int>()));
+    }
+    {
+        bhtools::Tany any(a2);
+        vlogd($(any.use<short>()) $(any.value<short>()));
+    }
+    {
+        bhtools::Tany any(a3);
+        vlogd($(any.use<size_t>()) $(any.value<size_t>()));
+    }
+    {
+        bhtools::Tany any(a4);
+        vlogd($(any.use<const char *>()) $(any.value<const char *>()));
+    }
+    {
+        bhtools::Tany any(a5);
+        vlogd($(any.use<std::string>()) $(any.value<std::string>()));
+    }
+    {
+        bhtools::Tany any(a6);
+        vlogd($(any.use<bool>()) $(any.value<bool>()));
+    }
+    {
+        bhtools::Tany any(a7);
+        vlogd($(any.use<char>()) $(any.value<char>()));
+    }
+    {
+        bhtools::Tany any(a8);
+        vlogd($(any.use<float>()) $(any.value<float>()));
+    }
+    {
+        bhtools::Tany any(a9);
+        vlogd($(any.use<double>()) $(any.value<double>()));
+    }
+
+
+
+    {
+        bhtools::Tany any(12344);
+        vlogd($(any.use<int>()) $(any.value<int>()));
+    }
+    {
+        bhtools::Tany any(66.666);
+        vlogd($(any.use<double>()) $(any.value<double>()));
+    }
+    {
+        bhtools::Tany any("hellow1");
+        vlogd($(any.use<const char *>()) $(any.value<const char *>()));
+    }
+    {
+        bhtools::Tany any(std::string("hellow2"));
+        vlogd($(any.use<std::string>()) $(any.value<std::string>()));
+    }
+
+}
+
+void test_4()
+{
+    // 指针转移和释放
+    {
+        T1 a1(999,"T1v");
+        bhtools::Tany any1(a1);
+        vlogd($(any1.use<T1>()) $(any1.value<T1>()._i) $(any1.value<T1>()._s));
+
+        bhtools::Tany any2(any1);
+        vlogd($(any2.use<T1>()) $(any2.value<T1>()._i) $(any2.value<T1>()._s));
+
+        bhtools::Tany any3 = a1;
+        vlogd($(any3.use<T1>()) $(any3.value<T1>()._i) $(any3.value<T1>()._s));
+
+        bhtools::Tany any4 = any1;
+        vlogd($(any4.use<T1>()) $(any4.value<T1>()._i) $(any4.value<T1>()._s));
+
+        bhtools::Tany any5(T1(9999,"T1vv"));
+        vlogd($(any5.use<T1>()) $(any5.value<T1>()._i) $(any5.value<T1>()._s));
+    }
+
+    // 拷贝复制
+    {
+        bhtools::Tany any1 = 123;
+        bhtools::Tany any2(456);
+
+        vlogd($(any1.use<int>()) $(any1.value<int>()));
+        vlogd($(any2.use<int>()) $(any2.value<int>()));
+
+        bhtools::Tany any3 = any1;
+        vlogd($(any3.use<int>()) $(any3.value<int>()));
+
+        any3 = any2;
+        vlogd($(any3.use<int>()) $(any3.value<int>()));
+    }
 }
 
 int main(int argc, char *argv[])
 {
     test_1();   
     test_2();   
-    // test_3();   
+    test_3();   
+    test_4();   
 
     return 0;
 }
