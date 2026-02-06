0a6b5469d95b7eb467e63bd227c22c772b3abf6e
BH <2655279792@qq.com>
2026-02-06 21:44:10 +0800

    change

--- test/main_Bdiv.cpp
+++ test/main_Bdiv.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "Bdiv.h"
+#include "Btime.h"
+
 #include "Ftest.h"
 
 void test_1()
@@ -90,12 +92,84 @@ void test_1()
     BHTEST_EQUAL(f5, "33");
 }
 
-void test_2() {}
+void test_2()
+{
+    auto a1 = bh::Bdiv("11##22##33##44")("##", 0, 1);
+    auto a2 = bh::Bdiv("11##22##33##44")("##", 0, 2);
+    auto a3 = bh::Bdiv("11##22##33##44")("##", 1, 2);
+    auto a4 = bh::Bdiv("11##22##33##44")("##", 1, 3);
+
+    auto b1 = bh::Bdiv("1#2#3#4")("#", 0, 0);
+    auto b2 = bh::Bdiv("1#2#3#4")("#", 1, 1);
+    auto b3 = bh::Bdiv("1#2#3#4")("#", 3, 3);
+    auto b4 = bh::Bdiv("1#2#3#4")("#", -1, -1);
+    auto b5 = bh::Bdiv("1#2#3#4")("#", -2, -2);
+    auto b6 = bh::Bdiv("1#2#3#4")("#", -4, -4);
+
+    auto c1 = bh::Bdiv("###111###222###333###444")("###", 1, 1);
+    auto c2 = bh::Bdiv("###111###222###333###444")("###", 4, 4);
+    auto c3 = bh::Bdiv("###111###222###333###444")("###", -1, -1);
+    auto c4 = bh::Bdiv("###111###222###333###444")("###", -4, -4);
+
+    vloga("多段分割");
+    BHTEST_EQUAL(a1, "11##22");
+    BHTEST_EQUAL(a2, "11##22##33");
+    BHTEST_EQUAL(a3, "22##33");
+    BHTEST_EQUAL(a4, "22##33##44");
+
+    vloga("单字符分割");
+    BHTEST_EQUAL(b1, "1");
+    BHTEST_EQUAL(b2, "2");
+    BHTEST_EQUAL(b3, "4");
+    BHTEST_EQUAL(b4, "4");
+    BHTEST_EQUAL(b5, "3");
+    BHTEST_EQUAL(b6, "1");
+
+    vloga("多字符分割");
+    BHTEST_EQUAL(c1, "111");
+    BHTEST_EQUAL(c2, "444");
+    BHTEST_EQUAL(c3, "444");
+    BHTEST_EQUAL(c4, "111");
+}
+
+void test_3()
+{
+    auto a1 = bh::Bdiv("[main_Bdiv.cpp:102][2026-02-06.21:25:19.664]")(".", 1, 1, ":", 0, 0);
+    auto a2 = bh::Bdiv("[main_Bdiv.cpp:102][2026-02-06.21:25:19.664]")("[", 2, 2, "-", 0, 0);
+    auto a3 = bh::Bdiv("[main_Bdiv.cpp:102][2026-02-06.21:25:19.664]")("[", 2, 2, "-", 1, 1);
+    auto a4 =
+        bh::Bdiv("[main_Bdiv.cpp:102][2026-02-06.21:25:19.664]")("cpp", 0, 0, "main_Bdiv", 1, 1);
+    auto a5 =
+        bh::Bdiv("[main_Bdiv.cpp:102][2026-02-06.21:25:19.664]")("main_Bdiv", 1, 1, "cpp", 0, 0);
+
+    vloga("精准分割");
+    BHTEST_EQUAL(a1, "cpp");
+    BHTEST_EQUAL(a2, "2026");
+    BHTEST_EQUAL(a3, "02");
+    BHTEST_EQUAL(a4, ".");
+    BHTEST_EQUAL(a5, ".");
+}
+
+void test_4()
+{
+    bh::int32 sum = 1000 * 10000;
+    bh::Btimel t1;
+    bh::Bstr s1;
+    for(bh::int32 i = 0; i < sum; i++) {
+        s1 = bh::Bdiv("##100##<<abc-hellow world-1992/4/3 15:12:54>>")("##", 2, 2, " ", 1, 1, "-",
+                                                                       1, 1);
+    }
+
+    vloga("速度测试");
+    vlogd($(sum) $(s1) $(t1.to_str()));
+}
 
 int main(int argc, char *argv[])
 {
     test_1();
-    // test_2();
+    test_2();
+    test_3();
+    test_4();
 
     return 0;
 }
